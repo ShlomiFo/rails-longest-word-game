01702da2428c8f64bc9e0ae4d28efdbce171203e
shlomi fogel <mm12g1@gmail.com>
2025-04-30 20:27:54 +0300

    added code to create array of letters and design

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,9 @@
 class GamesController < ApplicationController
   def new
-    @letters = (0...9).map { ('a'..'z').to_a[rand(26)] }.join
+    @letters = (0...9).map { ('A'..'Z').to_a[rand(26)] }.join.split("")
   end
 
   def score
-    
+
   end
 end
